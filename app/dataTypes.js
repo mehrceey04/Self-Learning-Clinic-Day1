70c6b59c36a7cb14f73e1383c2e56576f9c80d53
Halimat Mercy Oseni <mehrceey04@gmail.com>
2016-11-28 16:39:34 +0100

    Updated dataTypes.js

--- app/dataTypes.js
+++ app/dataTypes.js
@@ -1,27 +1,26 @@
 module.exports = {
-  dataTypes : function(data) {
-    if((data === null) || typeof(data) === 'undefined') {
+  dataTypes : function(value) {
+    if(typeof(value) === 'string') {
+      return value.length;
+    }
+    if((value === null) || typeof(value) === 'undefined') {
       return 'no value';
     }
-    if(typeof(data) === 'string') {
-      return data.length;
+    if(typeof(value) === 'boolean') {
+      return value;
     }
-  
-    if(typeof(data) === 'boolean') {
-      return data;
+    if(typeof(value) === 'number') {
+      if(value < 100) return 'less than 100';
+      if(value === 100) return 'equal to 100';
+      if(value > 100) return 'more than 100';
     }
-    if(typeof(data) === 'number') {
-      if(data < 100) return 'less than 100';
-      if(data === 100) return 'equal to 100';
-      if(data > 100) return 'more than 100';
-    }
-    if(typeof(data) === 'object' && data.length >= 3) {
-      return data[2];
+    if(typeof(value) === 'object' && value.length >= 3) {
+      return value[2];
     } else { 
       return 'undefined';
     }
-    if(typeof(data) === 'function') {
-    return data(true);  
+    if(typeof(value) === 'function') {
+    return value(true);  
     } 
   }
 }
